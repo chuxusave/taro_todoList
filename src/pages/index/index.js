fec2ae4e7992c3550c4e41010429e34282b0e6dd
chuxu <chuxu@chuxudeMacBook-Pro-2.local>
2019-10-09 15:45:04 +0800

    使用redux

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,10 @@
 import Taro, { Component } from "@tarojs/taro";
 import { View, Text, Input } from "@tarojs/components";
+import { connect } from "@tarojs/redux";
 import "./index.scss";
 
-export default class Index extends Component {
+import { add, del } from "../../actions/index";
+class Index extends Component {
   config = {
     navigationBarTitleText: "首页"
   };
@@ -10,9 +12,7 @@ export default class Index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // 创建一个初始的 Todolist
-      list: ["get up", "coding", "sleep"],
-      inputVal: ""
+      newTodo: ""
     };
   }
 
@@ -26,66 +26,76 @@ export default class Index extends Component {
 
   componentDidHide() {}
 
-  // 添加按钮 onClick 时，添加事项，然后更新 list
-  addItem() {
-    let { list } = this.state;
-    const inputVal = this.inputVal;
-    // 如果输入框的值为空，则返回，否则添加到事项列表里
-    console.log("inputVal is:", inputVal);
-    if (inputVal == "" || !inputVal) return;
-    else {
-      list.push(inputVal);
-    }
+  saveNewTodo(e) {
+    let { newTodo } = this.state;
+    if (!e.detail.value || e.detail.value === newTodo) return;
+
     this.setState({
-      list,
-      inputVal: ""
+      newTodo: e.detail.value
     });
   }
 
-  // 输入框 onInput 的时候，它的值暂存起来,不参与渲染的变量可不使用state储存，提高性能
-  inputHandler(e) {
-    this.inputVal = e.target.value;
+  addTodo() {
+    let { newTodo } = this.state;
+    let { add } = this.props;
+
+    if (!newTodo) return;
+
+    add(newTodo);
+    this.setState({
+      newTodo: ""
+    });
   }
 
-  // 根据索引删除事项，然后更新 list
-  delItem(index) {
-    let { list } = this.state;
-    list.splice(index, 1);
-    this.setState({
-      list
-    });
+  delTodo(id) {
+    let { del } = this.props;
+    del(id);
   }
 
   render() {
-    let { list, inputVal } = this.state;
+    // 获取未经处理的todos并展示
+    let { newTodo } = this.state;
+    let { todos, add, del } = this.props;
+
+    const todosJsx = todos.map(todo => {
+      return (
+        <View className="todos_item">
+          <Text>{todo.text}</Text>
+          <View className="del" onClick={this.delTodo.bind(this, todo.id)}>
+            -
+          </View>
+        </View>
+      );
+    });
 
     return (
-      <View className="index">
-        <Input
-          className="input"
-          type="text"
-          value={inputVal}
-          onInput={this.inputHandler.bind(this)}
-        />
-        <Text className="add" onClick={this.addItem.bind(this)}>
-          添加
-        </Text>
-        <View className="list_wrap">
-          <Text>Todo list</Text>
-          {list.map((item, index) => {
-            return (
-              <View>
-                <Text>
-                  {index + 1}.{item}
-                </Text>
-                <Text className="del" onClick={this.delItem.bind(this, index)}>
-                  删除
-                </Text>
-              </View>
-            );
-          })}
+      <View className="index todos">
+        <View className="add_wrap">
+          <Input
+            placeholder="填写新的todo"
+            onBlur={this.saveNewTodo.bind(this)}
+            value={newTodo}
+          />
+          <View className="add" onClick={this.addTodo.bind(this)}>
+            +
+          </View>
         </View>
+        <View>{todosJsx}</View>
       </View>
     );
   }
 }
+
+export default connect(
+  ({ todos }) => ({
+    todos: todos.todos
+  }),
+  dispatch => ({
+    add(data) {
+      dispatch(add(data));
+    },
+    del(id) {
+      dispatch(del(id));
+    }
+  })
+)(Index);
